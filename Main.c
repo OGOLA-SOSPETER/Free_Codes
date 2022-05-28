0e6b84edecb0674caf413366bc9ea42d5f2d3624
OGOLA-SOSPETER <captainsos483@gmail.com>
2022-05-28 10:50:19 +0300

    updates

--- Main.c
+++ Main.c
@@ -1,3 +1,3 @@
 This is a c file type.
-Getting stsrted [D[D[D[D[D[D[3~a[F[B[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[Cwith the bash terminal. 
+Getting started with the bash terminal.
 VIVA!!!
